781c988d2e60c272e9fb838c1628f8c4c22f6733
LAB113 <LAB113@INFO.RN>
2021-05-19 09:18:00 +0200

    first commit

---
+++ Bartolini.Liam.4H.XamAzure/Bartolini.Liam.4H.XamAzure/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Bartolini.Liam._4H.XamAzure.Models;
+using Newtonsoft.Json;
+
+namespace Bartolini.Liam._4H.XamAzure
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            List<Studente> studenti = new List<Studente>();
+
+            try
+            {
+                HttpClient client = new HttpClient();
+                string response = await client.GetStringAsync("https://flr.azurewebsites.net/studenti");
+                studenti = JsonConvert.DeserializeObject<List<Studente>>(response);
+            }
+            catch (Exception err)
+            {
+                await DisplayAlert("Ocio!!", err.Message, "Ok");
+            }
+
+            lvStudenti.ItemsSource = studenti;
+        }
+    }
+}
---
+++ Bartolini.Liam.4H.XamAzure/Bartolini.Liam.4H.XamAzure/Models/Studente.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bartolini.Liam._4H.XamAzure.Models
+{
+    public class Studente
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+    }
+}
